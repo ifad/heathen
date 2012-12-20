b5cf2b1008acb4d3a8e5fc4edf63e25fb3e0b65c
Peter Brindisi <peter.brindisi@gmail.com>
2012-12-20 17:16:27 +0100

    read all of the secrets

--- config/app/redis.rb
+++ config/app/redis.rb
@@ -7,10 +7,12 @@ module Heathen
       module Redis
         def self.configure(app)
           app.configure(:production) do
+            require "#{app.root}/config/secret"
             app.set :redis, ::Redis::Namespace.new(:heathen, ::Redis.new(host: 'persist.ifad.org', port: 6383, password: ENV['REDIS_PASSWORD']))
           end
 
           app.configure(:staging) do
+            require "#{app.root}/config/secret"
             app.set :redis, ::Redis::Namespace.new(:heathen, ::Redis.new(host: 'store.ifad.org', port: 6383, password: ENV['REDIS_PASSWORD']))
           end
 
