ed213a27120e21df86d439035f424b8e4ab81db3
Peter Brindisi <peter.brindisi@gmail.com>
2012-12-20 17:23:39 +0100

    configure redis properly

--- config/app/redis.rb
+++ config/app/redis.rb
@@ -8,16 +8,16 @@ module Heathen
         def self.configure(app)
           app.configure(:production) do
             require "#{app.root}/config/secret"
-            app.set :redis, ::Redis::Namespace.new(:heathen, ::Redis.new(host: 'persist.ifad.org', port: 6383, password: ENV['REDIS_PASSWORD']))
+            app.set :redis, ::Redis::Namespace.new(:heathen, redis: ::Redis.new(host: 'persist.ifad.org', port: 6383, password: ENV['REDIS_PASSWORD']))
           end
 
           app.configure(:staging) do
             require "#{app.root}/config/secret"
-            app.set :redis, ::Redis::Namespace.new(:heathen, ::Redis.new(host: 'store.ifad.org', port: 6383, password: ENV['REDIS_PASSWORD']))
+            app.set :redis, ::Redis::Namespace.new(:heathen, redis: ::Redis.new(host: 'store.ifad.org', port: 6383, password: ENV['REDIS_PASSWORD']))
           end
 
           app.configure(:development) do
-            app.set :redis, ::Redis::Namespace.new(:heathen, ::Redis.new)
+            app.set :redis, ::Redis::Namespace.new(:heathen, redis: ::Redis.new)
           end
 
           app.helpers do
