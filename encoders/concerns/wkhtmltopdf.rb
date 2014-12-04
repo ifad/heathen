f0698fd87947efaafcbdfc32c32fe914c0543624
Lleïr Borràs Metje <lleir@llegeix.me>
2014-12-04 21:14:08 +0100

    Fix wkhtmltopdf

--- encoders/concerns/wkhtmltopdf.rb
+++ encoders/concerns/wkhtmltopdf.rb
@@ -5,12 +5,7 @@ module Heathen
 
       def wkhtmltopdf(source)
         executioner.execute(Heathen::App.wkhtmltopdf,
-          '--page-size', 'Letter',
-          '--margin-top',    '0.75in',
-          '--margin-right',  '0.75in',
-          '--margin-bottom', '0.75in',
-          '--margin-left',   '0.75in',
-          '--encoding',      'UTF-8',
+          *_wkhtmltopdf_options(source),
           source,
           '-', # print results on stdout
           :binary => true)
@@ -19,6 +14,27 @@ module Heathen
           executioner.stdout
         end
       end
+
+      protected
+        def _wkhtmltopdf_options(source)
+          html = Nokogiri.parse(File.read(source))
+          defl = [
+            '--page-size',     'Letter',
+            '--margin-top',    '0.75in',
+            '--margin-right',  '0.75in',
+            '--margin-bottom', '0.75in',
+            '--margin-left',   '0.75in',
+            '--encoding',      'UTF-8'
+          ]
+
+          attrs = html.xpath("//meta[starts-with(@name, 'heathen')]").inject({}) do |h, meta|
+            name = meta.attributes['name'].value.sub(/^heathen/, '-')
+
+            h.merge(name => meta.attributes['content'].value)
+          end
+
+          attrs.inject(defl){ |d, (k,v)| d << [k, v] }.flatten
+        end
 
     end
 
