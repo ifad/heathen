fec0fb925d8a4f8a44a03d1b3c05c031f9ef77a1
Joe Blackman <j.blackman@ifad.org>
2015-02-03 15:58:41 +0100

    Remove dependency on external libreoffice and run locally

--- encoders/concerns/libreoffice.rb
+++ encoders/concerns/libreoffice.rb
@@ -4,31 +4,21 @@ module Heathen
 
       # returns a Tempfile instance.
       # calling method is responsible for closing/unlinking
-      def libreoffice(source, extension = :pdf)
-        file = Tempfile.new('heathen')
-        file.chmod 0666
-
-        temp_name = [ file.path, extension.to_s ].join('.')
-
-        FileUtils.ln(file.path, temp_name)
-
-        executioner.execute('python', "#{app.root}/bin/DocumentConverter.py",
-                            app.ooo_host, app.ooo_port, source, temp_name)
-
-        FileUtils.rm(temp_name)
-
-        file.close
+      def libreoffice(source_file, extension = :pdf)
+        executioner.execute(
+          'soffice',
+          '--headless',
+          '--convert-to', extension.to_s,
+          '--outdir', File.dirname(source_file),
+          source_file )
 
         if executioner.last_exit_status == 0
+          file = File.new( source_file.gsub(/\.[^.]*$/, ".#{extension.to_s}" ) )
           return file
         else
-          file.unlink
           return nil
         end
 
-      rescue
-        file.unlink
-        raise
       end
     end
   end
